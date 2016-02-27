1b8d491aafa6964a78a4b5f9534b89927e1c707b
Nate <npauzenga@gmail.com>
2016-02-27 16:32:29 -0500

    complete checkpoint 25

---
+++ scripts/album.js
@@ -0,0 +1,64 @@
+var albumAllHailWestTexas = {
+  title: "All Hail West Texas",
+  artist: "The Mountain Goats",
+  label: "Rough Trade",
+  year: "2001",
+  albumArtUrl: "assets/images/album_covers/01.png",
+  songs: [
+    { title: "Blue", duration: "4:26" },
+    { title: "Green", duration: "3:26" },
+    { title: "Red", duration: "4:16" },
+    { title: "Pink", duration: "4:24" },
+    { title: "Magenta", duration: "6:26" }
+  ]
+};
+
+var albumLifeAndDeath = {
+  title: "Life and Death of an American Fourtracker",
+  artist: "John Vanderslice",
+  label: "Barsuk",
+  year: "2003",
+  albumArtUrl: "assets/images/album_covers/01.png",
+  songs: [
+    { title: "Blue", duration: "4:26" },
+    { title: "Green", duration: "3:26" },
+    { title: "Red", duration: "4:16" },
+    { title: "Pink", duration: "4:24" },
+    { title: "Magenta", duration: "6:26" }
+  ]
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
+  var template =
+    '<tr class="album-view-song-item">'
+  + ' <td class="song-item-number">' + songNumber + '</td>'
+  + ' <td class="song-item-title">' + songName + '</td>'
+  + ' <td class="song-item-duration">' + songLength + '</td>'
+  + '</tr>'
+  ;
+
+  return template;
+};
+
+var setCurrentAlbum = function(album) {
+  var albumTitle =       document.getElementsByClassName('album-view-title')[0];
+  var albumArtist =      document.getElementsByClassName('album-view-artist')[0];
+  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+  var albumImage =       document.getElementsByClassName('album-cover-art')[0];
+  var albumSongList =    document.getElementsByClassName('album-view-song-list')[0];
+
+  albumTitle.firstChild.nodeValue = album.title;
+  albumArtist.firstChild.nodeValue = album.artist;
+  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+  albumImage.setAttribute('src', album.albumArtUrl);
+
+  albumSongList.innerHTML = '';
+
+  for(var i = 0, j = album.songs.length; i < j; i++) {
+    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+  }
+};
+
+window.onload = function() {
+  setCurrentAlbum(albumAllHailWestTexas);
+};
