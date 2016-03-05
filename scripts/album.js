f7d748b218620160032604441dad1ce9d6343872
Nate <npauzenga@gmail.com>
2016-03-05 14:11:43 -0500

    complete checkpoint 27

--- scripts/album.js
+++ scripts/album.js
@@ -59,10 +59,61 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var findParentByClassName = function(targetClass, element) {
+  var currentParent = element.parentElement;
+
+  if (currentParent == null) {
+    return element;
+  }
+
+  if (currentParent.className != targetClass) {
+    currentParent = currentParent.parentElement;
+    findParentByClassName(targetClass, currentParent);
+  }
+  return currentParent;
+};
+
+var getSongItem = function(element) {
+  switch(element.className) {
+    case 'album-song-button':
+    case 'ion-play':
+    case 'ion-pause':
+      return findParentByClassName('song-item-number', element);
+    case 'album-view-song-item':
+      return element.querySelector('.song-item-number');
+    case 'song-item-title':
+    case 'song-item-duration':
+      return findParentByClassName('album-view-song-item', element).querySelector('.song-item-number');
+    case 'song-item-number':
+      return element;
+    default:
+      return;
+  }
+};
+
+var clickHandler = function(targetElement) {
+  var songItem = getSongItem(targetElement);
+  if (currentlyPlayingSong === null) {
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+    songItem.innerHTML = playButtonTemplate;
+    currentlyPlayingSong = null;
+  } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+    var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+    currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  }
+};
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSong = null;
 
 window.onload = function() {
   setCurrentAlbum(albumAllHailWestTexas);
@@ -70,14 +121,29 @@ window.onload = function() {
   // switch to play button when hovering over rows
   songListContainer.addEventListener('mouseover', function(event) {
     if (event.target.parentElement.className === 'album-view-song-item') {
-      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      var songItem = getSongItem(event.target);
+
+      if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+        songItem.innerHTML = playButtonTemplate;
+      }
     }
   });
 
-  // switch back to track number when leaving row
   for (var i = 0, j = songRows.length; i < j; i++) {
+    // switch back to track number when leaving row
     songRows[i].addEventListener('mouseleave', function(event) {
-      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      var songItem = getSongItem(event.target);
+      var songItemNumber = songItem.getAttribute('data-song-number');
+
+      if (songItemNumber !== currentlyPlayingSong) {
+        songItem.innerHTML = songItemNumber;
+      } else {
+        songItem.innerHTML = pauseButtonTemplate;
+      }
+    });
+
+    songRows[i].addEventListener('click', function(event) {
+      clickHandler(event.target);
     });
   }
 };
