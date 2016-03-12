cd858a36749ed706b9b2f57e811dadfa516e3f55
Nate <npauzenga@gmail.com>
2016-03-12 13:00:07 -0500

    refactor using jQuery - WIP

--- scripts/album.js
+++ scripts/album.js
@@ -51,7 +51,58 @@ var createSongRow = function(songNumber, songName, songLength) {
   + '</tr>'
   ;
 
-  return $(template);
+  var $row = $(template);
+
+  var clickHandler = function() {
+    var $songItem = $(this).find('.song-item-number');
+    var $songItemNumber = $songItem.attr('data-song-number');
+
+    // if there isn't a song playing, set the clicked song item number to pause
+    if (currentlyPlayingSong === null) {
+      $songItem.html(pauseButtonTemplate);
+      currentlyPlayingSong = $songItemNumber
+
+    // if the song that's playing is the song we've clicked, change to play button
+    } else if (currentlyPlayingSong === $songItemNumber) {
+      $songItem.html(playButtonTemplate);
+      currentlyPlayingSong = null;
+
+    // if the song that's playing is not the song we've clicked, change the clicked song's number
+    // to pause button and change the previously playing song's pause button to number
+    } else if (currentlyPlayingSong !== $songItemNumber) {
+      var $currentlyPlayingSongElement = $('[data-song-number="' + currentlyPlayingSong + '"]');
+      $currentlyPlayingSongElement.html($currentlyPlayingSongElement.attr('data-song-number'));
+      $songItem.html(pauseButtonTemplate);
+      currentlyPlayingSong = $songItemNumber;
+    }
+  };
+
+  var onHover = function(event) {
+    // we don't need to check that the parent element matches ".album-view-song-item"
+    //   because we're calling onHover on $row, which represents "this" below
+    //   and $row will always have a the same structure (see createSongRow)
+    var $songItem = $(this).find(".song-item-number");
+    var songItemNumber = $songItem.attr('data-song-number');
+
+    if (songItemNumber !== currentlyPlayingSong) {
+      $songItem.html(playButtonTemplate);
+    }
+  };
+
+  var offHover = function(event) {
+    var $songItem = $(this).find(".song-item-number");
+    var songItemNumber = $songItem.attr('data-song-number');
+
+    if (songItemNumber !== currentlyPlayingSong) {
+      $songItem.html(songItemNumber);
+    }
+  };
+
+  // This is how it's setup in the checkpoint --
+  //   $row.find('.song-item-number').click(clickHandler);
+  $row.click(clickHandler);
+  $row.hover(onHover, offHover);
+  return $row;
 };
 
 var setCurrentAlbum = function(album) {
@@ -74,130 +125,13 @@ var setCurrentAlbum = function(album) {
   }
 };
 
-var child = document.getElementsByClassName('album-view-title')[0];
-var noParent = document.querySelector('html');
-
-var findParentByClassName = function(targetClass, element) {
-  var currentParent = element.parentElement;
-
-  if (currentParent) {
-    while (currentParent.className && currentParent.className != targetClass) {
-      currentParent = currentParent.parentElement;
-    }
-
-    if (currentParent.className == targetClass) {
-      return currentParent;
-    } else {
-      alert("no parent found with that class name");
-    }
-  } else {
-    alert("no parent found");
-  }
-};
-
-// AF-315: Add a function to get the songItem by element
-var getSongItem = function(element) {
-  // summary:
-  //   Gets a song item based on a provided element
-  // element: Node
-  //   Element related to the song item node in some way
-  // returns: Node
-  switch(element.className) {
-    // Purposefully falling through: Children of song-item-number
-    case 'album-song-button':
-    case 'ion-play':
-    case 'ion-pause':
-      return findParentByClassName('song-item-number', element);
-    case 'album-view-song-item': // falling through because parent
-      return element.querySelector('.song-item-number');
-    case 'song-item-title':// falling through because sibling
-    case 'song-item-duration':
-      return findParentByClassName('album-view-song-item', element).querySelector('.song-item-number');
-    case 'song-item-number':
-      return element;
-    default:
-      return;
-  }
-};
-
-var clickHandler = function(targetElement) {
-  var songItem = getSongItem(targetElement);
-  if (currentlyPlayingSong === null) {
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-    songItem.innerHTML = playButtonTemplate;
-    currentlyPlayingSong = null;
-  } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-    var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-    currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  }
-};
-
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
 var currentlyPlayingSong = null;
 
-window.onload = function() {
+$(document).ready(function() {
   setCurrentAlbum(albumAllHailWestTexas);
-
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albums = [albumAllHailWestTexas, albumLifeAndDeath, albumKnowByHeart];
-  var index = 0;
-
-  // listen for click on album cover
-  // when clicked cycle album object
-
-  albumImage.addEventListener("click", function(event) {
-    // if the index reaches the end of our albums collection,
-    // start back at the top. Otheriwse, go to the next
-    (index == (albums.length - 1)) ? index = 0 : index++;
-
-    setCurrentAlbum(albums[index]);
-  });
-
-  // switch to play button when hovering over rows
-  songListContainer.addEventListener('mouseover', function(event) {
-    if (event.target.parentElement.className === 'album-view-song-item') {
-      var songItem = getSongItem(event.target);
-
-      if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-        songItem.innerHTML = playButtonTemplate;
-      }
-    }
-  });
-
-  // mouseleave does not bubble
-  // that's why there's a loop
-  // mouseout does bubble
-  // and acts in a very similar fashion
-  //
-  // homework: change the functions below to use event delegation:
-  // for both the mouseleave (which should be mouseout) and the click
-
-  for (var i = 0, j = songRows.length; i < j; i++) {
-    // switch back to track number when leaving row
-    songRows[i].addEventListener('mouseleave', function(event) {
-      var songItem = getSongItem(event.target);
-      var songItemNumber = songItem.getAttribute('data-song-number');
-
-      if (songItemNumber !== currentlyPlayingSong) {
-        songItem.innerHTML = songItemNumber;
-      } else {
-        songItem.innerHTML = pauseButtonTemplate;
-      }
-    });
-
-    songRows[i].addEventListener('click', function(event) {
-      clickHandler(event.target);
-    });
-  }
-};
+});
 
 
